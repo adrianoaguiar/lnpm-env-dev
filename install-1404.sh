f44369b11b3c847e5f7dff1bb0195f668ca77b78
SergeyCherepanov <sergey@cherepanov.org.ua>
2015-09-11 00:16:18 +0400

    installer update

--- install-1404.sh
+++ install-1404.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env bash
-# Ignore the questions
 export DEBIAN_FRONTEND=noninteractive
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
 TMPDIR=/tmp/magentoenvironmentconfiguration
+# Add nginx repo
+add-apt-repository -y ppa:nginx/stable
+
 # Update
 # --------------------
 apt-get update
 apt-get -y upgrade
-# Mysql
-# --------------------
+
 # Install MySQL quietly
 apt-get -q -y install mysql-server
+
 # Install nginx + varnish + php-fpm
-apt-get install -y git mysql-client vim nginx php5-fpm php5-cli php5-dev php5-mysql php5-curl php5-gd php5-mcrypt php5-sqlite php5-xmlrpc php5-xsl php5-common php5-intl
+apt-get install -q -y git git-lfs mysql-client vim nginx php5-fpm php5-cli php5-dev php5-mysql php5-curl php5-gd php5-mcrypt php5-sqlite php5-xmlrpc php5-xsl php5-common php5-intl
 php5enmod mcrypt
+
+# Install composer
+curl -s https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer.phar
+sudo ln -s /usr/local/bin/composer.phar /usr/local/bin/composer
+
+# Clone configs
 git clone  https://github.com/SergeyCherepanov/lnpm-env-dev.git $TMPDIR
 cd $TMPDIR
 
