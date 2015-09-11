bbb8ad1fee37ba57a0d09bb8feb833be7835c5e6
SergeyCherepanov <sergey@cherepanov.org.ua>
2015-09-12 01:45:07 +0400

    installer update

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 Linux + Nginx + Php-fpm + MySql Dev Environment for Magento, Symfony, Laravel and others
 ===============================
 
-## Ubuntu Installation 14.04:
+## What will be installed
+
+1 .nginx - 1.8.0
+2. php - 5.5 with intl, mcrypt, pdo, curl, gd, sqlite, xmlrpc, xsl
+3. xhprof - (php profiler, for enable in project just add "xhprof" param to query string in url, example: http://myapp.loc?xhprof)
+4. percona-server - 5.6
+5. composer - latest
+6. git
+
+
+
+## Installation - Ubuntu 14.04:
 
 ```bash
-$ sudo su
-$ bash <(wget -nv -O - https://raw.githubusercontent.com/SergeyCherepanov/lnpm-env-dev/master/install-1404.sh)
+$ wget -nv -O - https://raw.githubusercontent.com/SergeyCherepanov/lnpm-env-dev/master/install-1404.sh | sudo bash
 ```
 
 ## Usage:
--- install-1404.sh
+++ install-1404.sh
@@ -5,46 +5,99 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
 TMPDIR=/tmp/lnpm-env-dev
-# Add nginx repo
+
+# Nginx repo
 add-apt-repository -y ppa:nginx/stable
+
+# Graphviz repo
+apt-add-repository -y ppa:dperry/ppa-graphviz-test
+
+# Percona repo
+apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
+echo "deb http://repo.percona.com/apt "$(lsb_release -sc)" main" | tee /etc/apt/sources.list.d/percona.list
+echo "deb-src http://repo.percona.com/apt "$(lsb_release -sc)" main" | tee -a /etc/apt/sources.list.d/percona.list
 
 # Update
 # --------------------
 apt-get update
 apt-get -y upgrade
 
-# Install MySQL quietly
-apt-get -q -y install mysql-server
+# Install Percona-Server
+apt-get -q -y install percona-server-server-5.6
 
 # Install nginx + varnish + php-fpm
-apt-get install -q -y git mysql-client vim nginx php5-fpm php5-cli php5-dev php5-mysql php5-curl php5-gd php5-mcrypt php5-sqlite php5-xmlrpc php5-xsl php5-common php5-intl
+apt-get install -q -y git mysql-client nginx php5-fpm php5-cli php5-dev php5-mysql php5-curl php5-gd \
+php5-mcrypt php5-sqlite php5-xmlrpc php5-xsl php5-common php5-intl
+
+# Install xhprof
+pecl install -f xhprof
+
+# Enabling mcrypt
 php5enmod mcrypt
+
+# Install graphviz
+apt-get autoremove -y graphviz libpathplan4
+apt-get install graphviz
 
 # Install composer
 curl -s https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer.phar
-sudo ln -s /usr/local/bin/composer.phar /usr/local/bin/composer
+mv composer.phar /usr/local/bin/composer.phar
+ln -s /usr/local/bin/composer.phar /usr/local/bin/composer
 
 # Clone configs
 git clone  https://github.com/SergeyCherepanov/lnpm-env-dev.git $TMPDIR
 cd $TMPDIR
 
-# Prepare environment config
+# Prepare environment configs
 # --------------------
 cp ./conf/nginx/sites-available/dev /etc/nginx/sites-available/dev
 
 unlink /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/dev /etc/nginx/sites-enabled/dev
 
-cp ./conf/mysql/my.cnf /etc/mysql/my.cnf
-cp ./conf/php/php.ini /etc/php5/fpm/php.ini
-mkdir /var/www
-chown www-data:www-data /var/www
+cp ./conf/mysql/my.cnf   /etc/mysql/my.cnf
+cp ./conf/php/php.ini    /etc/php5/fpm/php.ini
+cp ./conf/php/xhprof.ini /etc/php5/mods-available/xhprof.ini
+ln -s /etc/php5/mods-available/xhprof.ini /etc/php5/fpm/conf.d/20-xhprof.ini
+
 rm /var/lib/mysql/ibdata1
 rm /var/lib/mysql/ib_logfile0
 rm /var/lib/mysql/ib_logfile1
+
+mkdir /var/www
+chown www-data:www-data /var/www
+chown www-data:www-data -R /usr/share/php/xhprof_html
+
+cat <<EOF  | tee /var/www/.xhprof-header.php
+<?php
+
+if (&& extension_loaded('xhprof') && isset(\$_GET['xhprof'])) {
+    require '/usr/share/php/xhprof_lib/utils/xhprof_lib.php';
+    require '/usr/share/php/xhprof_lib/utils/xhprof_runs.php';
+    xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+}
+
+EOF
+
+cat <<EOF  | tee /var/www/.xhprof-footer.php
+<?php
+if (isset(\$_GET['xhprof']) && extension_loaded('xhprof')) {
+    \$profiler_namespace = 'myapp';  // namespace for your application
+    \$xhprof_data = xhprof_disable();
+    \$xhprof_runs = new XHProfRuns_Default();
+    \$run_id = \$xhprof_runs->save_run(\$xhprof_data, \$profiler_namespace);
+
+    // url to the XHProf UI libraries (change the host name and path)
+    \$profiler_url = sprintf('/xhprof/index.php?run=%s&amp;source=%s', \$run_id, \$profiler_namespace);
+    echo '<a href="'. \$profiler_url .'" target="_blank">Profiler output</a>';
+}
+
+EOF
+
+# Restart service
 service nginx restart
 service php5-fpm restart
 service mysql restart
 
+# Cleanup
 rm -rf $TMPDIR
